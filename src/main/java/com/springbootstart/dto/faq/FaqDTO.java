675de51c10e1bc9b60f9a787e9f54980c0f60cd1
RosieOh <20172207@gm.ac.kr>
2023-12-06 14:54:20 +0900

    🌈feat : faq 게시판 엔티티, DTO 완🌈

---
+++ src/main/java/com/springbootstart/dto/faq/FaqDTO.java
@@ -0,0 +1,21 @@
+package com.springbootstart.dto.faq;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FaqDTO {
+
+    private Long fno;
+
+    private String question;
+
+    private String answer;
+
+    private int cnt;
+}
