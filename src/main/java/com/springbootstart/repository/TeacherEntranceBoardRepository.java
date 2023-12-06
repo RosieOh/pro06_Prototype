de24aec7bfc4feec836bd2a49137b20a2615a9f7
RosieOh <20172207@gm.hannam.ac.kr>
2023-12-06 14:48:56 +0900

    🌈feat : Entrace 게시판 서비스, 비즈니스 계층 완🌈

---
+++ src/main/java/com/springbootstart/repository/TeacherEntranceBoardRepository.java
@@ -0,0 +1,12 @@
+package com.springbootstart.repository;
+
+import com.springbootstart.domain.TeacherEntranceBoard;
+import com.springbootstart.service.search.BoardSearch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TeacherEntranceBoardRepository extends JpaRepository<TeacherEntranceBoard, Long>, BoardSearch {
+
+    @Query(value = "select now()", nativeQuery = true)
+    String getTime();
+}
