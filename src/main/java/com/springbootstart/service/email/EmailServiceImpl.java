018c0a64df8ac9e97fcf227ac79f8d19abb95c99
RosieOh <20172207@gm.hannam.ac.kr>
2023-12-12 17:01:19 +0900

    csrf 토큰 고치고 게시판 고치고

--- src/main/java/com/springbootstart/service/email/EmailServiceImpl.java
+++ src/main/java/com/springbootstart/service/email/EmailServiceImpl.java
@@ -14,11 +14,15 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     JavaMailSender emailSender;
 
-    public static final String ePw = createKey();
+    public final String ePw;
 
-    private MimeMessage createMessage(String to)throws Exception{
-        System.out.println("보내는 대상 : "+ to);
-        System.out.println("인증 번호 : "+ePw);
+    public EmailServiceImpl() {
+        this.ePw = createKey();
+    }
+
+    private MimeMessage createMessage(String to) throws Exception {
+        System.out.println("보내는 대상 : " + to);
+        System.out.println("인증 번호 : " + ePw);
         MimeMessage  message = emailSender.createMimeMessage();
 
         message.addRecipients(MimeMessage.RecipientType.TO, to);//보내는 대상
@@ -44,41 +48,37 @@ public class EmailServiceImpl implements EmailService {
         return message;
     }
 
-    public static String createKey() {
+    private static String createKey() {
         StringBuffer key = new StringBuffer();
         Random rnd = new Random();
 
-        for (int i = 0; i < 8; i++) { // 인증코드 8자리
-            int index = rnd.nextInt(3); // 0~2 까지 랜덤
+        for (int i = 0; i < 8; i++) {
+            int index = rnd.nextInt(3);
 
             switch (index) {
                 case 0:
                     key.append((char) ((int) (rnd.nextInt(26)) + 97));
-                    //  a~z  (ex. 1+97=98 => (char)98 = 'b')
                     break;
                 case 1:
                     key.append((char) ((int) (rnd.nextInt(26)) + 65));
-                    //  A~Z
                     break;
                 case 2:
                     key.append((rnd.nextInt(10)));
-                    // 0~9
                     break;
             }
         }
         return key.toString();
     }
+
     @Override
-    public String sendSimpleMessage(String to)throws Exception {
-        // TODO Auto-generated method stub
+    public String sendSimpleMessage(String to) throws Exception {
         MimeMessage message = createMessage(to);
-        try{//예외처리
+        try {
             emailSender.send(message);
-        }catch(MailException es){
-            es.printStackTrace();
-            throw new IllegalArgumentException();
+        } catch (Exception ex) {
+            ex.printStackTrace(); // 또는 로깅 프레임워크를 사용하여 로그에 출력
+            throw new Exception("Failed to send email", ex);
         }
         return ePw;
     }
-
 }
